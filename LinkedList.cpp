c140592fbe59771e377071dd0a691a18e5229f22
Michal Zan <mzan@moravia.com>
2019-11-25 09:08:41 +0100

    HackerRank - linked list

---
+++ LinkedList.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstddef>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+class Solution{
+public:
+
+    Node* insert(Node *head,int data)
+    {
+        Node* node = new Node(data);
+        if (head == nullptr) {
+            head = node;
+        } else if (head->next == nullptr) {
+            head->next = node;
+        } else {
+            insert(head->next, data);
+        }
+        return head;
+    }
+
+    void display(Node *head)
+    {
+        Node *start=head;
+        while(start)
+        {
+            cout<<start->data<<" ";
+            start=start->next;
+        }
+    }
+};
+int main()
+{
+    Node* head=NULL;
+    Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }
+    mylist.display(head);
+
+}
